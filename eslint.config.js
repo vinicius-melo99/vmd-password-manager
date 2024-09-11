2ad1b663967b3a0cacc4349b5e79bb3be1f3749d
Vinicius Melo <viniciusmelo317@gmail.com>
2024-09-11 13:12:34 -0300

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact, { rules } from "eslint-plugin-react";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  rules: {
+    "react/react-in-jsx-scope": "off"
+  }
+];
